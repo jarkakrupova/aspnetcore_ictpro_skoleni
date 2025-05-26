98c17eb09985473ed2bf8afb75256917a09789d0
jarka <75241806+jarkakrupova@users.noreply.github.com>
2025-05-26 13:16:27 +0200

    minimalAPI přidáno

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,26 +1,35 @@
+using Microsoft.Extensions.Logging.Abstractions;
+
+//pro malá API, kde není nutnıch víc controllerù
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//!!!POZOR!!! v .NET 9 není pøidanı Swagger, chybí balíèek Swashbuckle 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. - kudy prochází request
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection();// zaškrtli jsme, e chceme HTTPS
+//veškerı kód appky je v 1 souboru
 
+
+//kod, ktery je bezne ve WeatherForecastControlleru
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
+//rucni mapovani, co se ma dit, kdyz prijde dotaz na https://...../weatherforecast
+//https://localhost:44344/weatherforecast 
+app.MapGet("/weatherforecast", () => {
+    //Enumerable.Range vrati 1-5
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
@@ -31,11 +40,18 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetWeatherForecast"); //tohle pojmenovani je nepovinne
 
-app.Run();
+app.MapGet("/", () => $"API bìí, nyní je {DateTime.Now.ToLongDateString()}"); 
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+
+app.Run(); //spusti appku
+
+//record je alternativa tridy, umi automaticky vypsat ToString
+//neni spatne, kdyz WeatherForecast bude trida
+//podobne jako struct
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) 
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }

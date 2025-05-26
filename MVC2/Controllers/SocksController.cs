3122f5d0d0ec8d153e7dd6e3925059f0346a2ea3
jarka <75241806+jarkakrupova@users.noreply.github.com>
2025-05-26 18:17:48 +0200

    opravena chyba v metodě SearchByPrice - metoda vracela všechny ponožky místo vyfiltrovaných

--- MVC2/Controllers/SocksController.cs
+++ MVC2/Controllers/SocksController.cs
@@ -7,19 +7,21 @@ namespace MVC.Controllers {
     [Route("[controller]")]
     public class SocksController : Controller {
         public IActionResult Index() {
-            return View(SockDataset.GetSocks());
+            return View(SockDataset.Socks);
         }
         [Route("[action]/{id}")]
         public IActionResult Details(int id) {
-            var data = SockDataset.GetSocks();
+            var data = SockDataset.Socks;
             var sock = data.FirstOrDefault(sock => sock.Id == id);
             return View(sock);
         }
-        [Route("[action]/min/{minPrice:int}/max/{maxPrice:int}")]
+        //[Route("[action]/min/{minPrice:int}/max/{maxPrice:int}")]
+        [Route("[action]")]
         public IActionResult SearchByPrice(int minPrice, int maxPrice) {
-            var data = SockDataset.GetSocks();
+            var data = SockDataset.Socks;
             var socks = data.Where(sock => sock.Price >= minPrice && sock.Price <= maxPrice);
-            return View(socks);
+            //return View(socks);
+            return View("Index", socks); //technicky muzu zrecykovat i jine view, akorat to bude s filtrem
         }
     }
 }
--- MVC2/Data/SockDataset.cs
+++ MVC2/Data/SockDataset.cs
@@ -4,14 +4,16 @@ using MVC.Models;
 namespace MVC.Data {
     public class SockDataset {
         static string[] brands = ["Nike", "Icebreaker", "Reebok", "Adidas"];
-        public static IEnumerable<Socks> GetSocks() {
+
+        public static List<Socks> Socks = GetSocks();
+        public static List<Socks> GetSocks() {
             return Enumerable.Range(1, 10).Select(index => new Socks() {
                 Id = index,
                 Brand = brands[Random.Shared.Next(brands.Length)],
                 Size = (Size)Random.Shared.Next(3),
                 Price = Random.Shared.Next(50, 500),
                 OnStock = Random.Shared.Next(0, 20)
-            }).ToArray();
+            }).ToList();
         }
     }
 }

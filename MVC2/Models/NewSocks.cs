5ff293d8d362c0066a9f00706a734040aeb60e6f
jarka <75241806+jarkakrupova@users.noreply.github.com>
2025-05-27 11:27:36 +0200

    přidán druhý controller - vygenerovaný s views

---
+++ MVC2/Models/NewSocks.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models {
+    public class NewSocks {
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string Brand { get; set; }
+        public Size Size { get; set; }
+        public decimal Price { get; set; }
+        [Range(0,100, ErrorMessage ="Rozsah je od 0 do 100")]
+        public int OnStock { get; set; }
+    }
+}
